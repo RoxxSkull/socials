4b3e090e6dd8c7225efebb4c734583dbcf116c10
Roxy Gray <roxygray@Roxys-MacBook-Air.local>
2025-06-28 17:41:47 -0500

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './header/header';
+import {Links} from './links/links';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Links],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
